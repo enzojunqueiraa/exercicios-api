87e2b89eaccddbb67f8a210048fa6da8ec1ee6d5
enzojunqueiraa <enzobjunqueira@gmail.com>
2023-08-01 10:04:23 -0300

    Exercicio Oito

---
+++ app/Http/Controllers/ExercicioOitoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExercicioOitoController extends Controller
+{
+    public function tabuada(Request $request){
+        
+        $numeros = []; 
+
+        for ($i = 1; $i <= 10; $i++) {
+            $resultado = $i*$request->entrada;
+            $concatenado = $request->entrada . ' x ' .  $i . ' = ' . $resultado;
+          array_push($numeros, $concatenado);
+        }
+
+        return json_encode([
+            'valores'=> $numeros
+        ]);
+    }
+
+}
