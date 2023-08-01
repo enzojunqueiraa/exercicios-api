7ba8c668a3412208747b92e90c065c925e7c911e
enzojunqueiraa <enzobjunqueira@gmail.com>
2023-08-01 15:14:59 -0300

    Atualizado as rotas

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ExercicioCincoController;
+use App\Http\Controllers\ExercicioDezController;
 use App\Http\Controllers\ExercicioDoisController;
+use App\Http\Controllers\ExercicioDozeController;
+use App\Http\Controllers\ExercicioNoveController;
+use App\Http\Controllers\ExercicioOitoController;
+use App\Http\Controllers\ExercicioQuatroController;
+use App\Http\Controllers\ExercicioSeisController;
+use App\Http\Controllers\ExercicioSeteController;
 use App\Http\Controllers\ExercicioTresController;
+use App\Http\Controllers\ExercicioTrezeController;
 use App\Http\Controllers\ExercicioUmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -32,3 +41,15 @@ Route::post('exercicio/oito',
 
 Route::post('exercicio/nove', 
 [ExercicioNoveController::class, 'divisivel']);
+
+Route::get('exercicio/doze', 
+[ExercicioDozeController::class, 'exibirNumeros']);
+
+Route::post('exercicio/dez', 
+[ExercicioDezController::class, 'exibirNumeros']);
+
+Route::post('exercicio/treze', 
+[ExercicioTrezeController::class, 'dividirInformar']);
+
+
+
