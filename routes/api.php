8f6edac9f0b56a66899e87157b44ac451c0c030e
enzojunqueiraa <enzobjunqueira@gmail.com>
2023-08-03 12:23:03 -0300

    Rotas Atualizadas

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\ExercicioTrezeController;
 use App\Http\Controllers\ExercicioUmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+ 
 Route::post('exercicio/um', 
 [ExercicioUmController::class, 'multiplicar']);
 
